5fa56b8695d19fbed4cefb151303e8e457c6ca70
adamkovari <kovari.adam@szmacscs.hu>
2022-07-11 15:00:58 +0200

    Updates

---
+++ precache-manifest.8b61218750a967d2475e5541550be8da.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df714a0394a0da06e86da5bd71922cb",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "3307024895b1212db99d",
+    "url": "/cv/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "1a65a7da1e8939f3bc20",
+    "url": "/cv/static/js/2.0e128136.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/cv/static/js/2.0e128136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3307024895b1212db99d",
+    "url": "/cv/static/js/main.7b820983.chunk.js"
+  },
+  {
+    "revision": "b5c9e66b30104e830b37",
+    "url": "/cv/static/js/runtime-main.7b1a339b.js"
+  },
+  {
+    "revision": "6b3beb72888af9ab56d7d7c1b3373848",
+    "url": "/cv/static/media/geolink_logo.6b3beb72.svg"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/cv/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "bf27938cd8d7137a71a0d56f900efca3",
+    "url": "/cv/static/media/maze_logo_white.bf27938c.svg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/cv/static/media/resume.9d3da0e2.pdf"
+  }
+]);
