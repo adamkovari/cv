c160f248c67a18408db8fa62d75229622e9efffa
adamkovari <kovari.adam@szmacscs.hu>
2022-07-28 19:00:28 +0200

    stars size

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/adamkovari/adamkovari.github.io.git/cv";
 
 ghpages.publish(
   pathname,
